88631cdf3ae6340c9359117b8ccb46614f79b243
Lawrence Luk <lluk@ucsd.edu>
2014-04-12 22:01:08 -0700

    Allow credentials

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -115,6 +115,7 @@ class GameController < ApplicationController
 	    headers['Access-Control-Allow-Origin'] = '*'
 	    headers['Access-Control-Expose-Headers'] = 'ETag'
 	    headers['Access-Control-Allow-Methods'] = 'GET, POST, PATCH, PUT, DELETE, OPTIONS, HEAD'
+	    headers['Access-Control-Allow-Credentials'] = 'true'
 	    headers['Access-Control-Allow-Headers'] = '*,x-requested-with,Content-Type,If-Modified-Since,If-None-Match'
 	    headers['Access-Control-Max-Age'] = '86400'
 	  end
